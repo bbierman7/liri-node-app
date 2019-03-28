938b1593d8a69512d18268e582f6334c8f797904
bbierman <bbierman7@gmail.com>
2019-03-27 19:33:08 -0500

    movie and spotify updates

--- liri.js
+++ liri.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
-
+// var keys = require("./keys.js");
+// var spotify = new Spotify(keys.spotify);
 var figlet = require('figlet');
 const chalkAnimation = require('chalk-animation');
 const axios = require("axios");
@@ -39,17 +40,18 @@ function App(command, params) {
         case "spotify-this-song":
             getMySong(params);
             break;
-        case movie-this(params):
-            // code block
+        case "movie-this":
+            getMyMovie(params);
             break;
-        case "do-what-it-says":
-            doWhatISay();
-            break;
+        // case "do-what-it-says":
+        //     doWhatISay();
+        //     break;
         default:
             console.log("I dont know that command")
     }
 };
 
+//Band
 function getMyBand(params) {
 // function getMyBand(artists) {
     var queryURL = "https://rest.bandsintown.com/artists/" + params + "/events?app_id=codingbootcamp";
@@ -73,12 +75,12 @@ function getMyBand(params) {
         });
 };
 
+//Spotify
+function getMySong(params){
+var keys = require("./keys.js");
+var spotify = new Spotify(keys.spotify);
+
 var Spotify = require('node-spotify-api');
- 
-var spotify = new Spotify({
-  id: "9c16f68fc27c4260a12d6d608d7769d8",
-  secret: "1b1dacd030c54078bb865f1c419d81db"
-});
  
 spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
   if (err) {
@@ -87,6 +89,41 @@ spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, d
  
 console.log(data); 
 });
-//Spotify
-// Client ID 9c16f68fc27c4260a12d6d608d7769d8
-// Client Secret 1b1dacd030c54078bb865f1c419d81db
+};
+
+
+//OMDB
+function getMyMovie(params){
+    var axios = require("axios");
+    var nodeArgs = process.argv;
+    var movieName = "";
+
+    for(var i = 2; i < nodeArgs.length; i++){
+        if (i > 2 && i < nodeArgs.length){
+            movieName = movieName + "+" + nodeArgs[i];
+        }
+        else{
+            movieName += nodeArgs[i];
+        }
+    }
+    var queryUrl = "http://www.omdbapi.com/?t=" + params + "&y=&plot=short&apikey=trilogy";
+    console.log(queryURL);
+
+    axios.get(queryURL).then(
+        function(response){
+            console.log("Release Year: " + response.data.Year);
+        }
+    )
+    .catch(function (error) {
+        console.log(error);
+    });
+};
+
+// * Title of the movie.
+// * Year the movie came out.
+// * IMDB Rating of the movie.
+// * Rotten Tomatoes Rating of the movie.
+// * Country where the movie was produced.
+// * Language of the movie.
+// * Plot of the movie.
+// * Actors in the movie.
